1e946bea1d7dd0690e058110aaeac752c63f3435
Mohith <mohith1754singh@gmail.com>
2024-04-21 11:43:21 +0530

    new project in which it is created using react route

--- src/index.js
+++ src/index.js
@@ -3,11 +3,37 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Layout from './Layout';
+import Home from './components/home/Home';
+import About from './components/about/About';
+import Contact from './components/contact/Contact';
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <Layout/>,
+    children: [ 
+      {
+        path: '',
+        element: <Home/>
+      },
+      {
+        path: 'about',
+        element: <About/>
+      },
+      {
+        path: 'contact',
+        element:<Contact/>
+      }
+    ]
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
